3a745ebe5cc63e55e64765e587d925b27a05d1f5
Mykola Vashchuk <mmykolavashchuk@gmail.com>
2026-02-10 10:29:19 +0200

    Refactor payment method interface and update main execution flow

--- src/main/java/PW5_Vashchuk/InterfaceTask.java
+++ src/main/java/PW5_Vashchuk/InterfaceTask.java
@@ -3,17 +3,19 @@ package PW5_Vashchuk;
 public class InterfaceTask {
     public static void main(String[] args) {
         run();
-        MultiNotifier M = new MultiNotifier();
-        M.send("Hello World!");
+        //2 Завдання
         DailyReport d = new DailyReport();
         d.fetchData();
         d.format();
+        //3 Завдання
+        MultiNotifier M = new MultiNotifier();
+        M.send("Hello World!");
     }
 
     static void run() {
         PaymentMethod[] methods = {new CardPayment(), new PayPalPayment()};
-
-
+        PaymentMethod card = new CardPayment();
+        card.payWithFee(100,500);
         PaymentProcessor processor = new PaymentProcessor();
         int amount = 15;
 
@@ -21,5 +23,6 @@ public class InterfaceTask {
             boolean okay = processor.process(m, amount);
             System.out.println(m.name() + ": " + okay);
         }
+
     }
 }
--- src/main/java/PW5_Vashchuk/PaymentMethod.java
+++ src/main/java/PW5_Vashchuk/PaymentMethod.java
@@ -4,7 +4,7 @@ interface PaymentMethod {
     String name();
     boolean pay (int amount);
 
-    default boolean payWithFee(int amount, int fee) {return pay(amount+fee);}
+    default void payWithFee(int amount, int fee) {pay(amount+fee);}
 
     static boolean isValidAmount(int amount) {return amount > 0;}
 }
