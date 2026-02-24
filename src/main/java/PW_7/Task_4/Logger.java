692209264b3ba2997dd6d85d1d40c035069f5448
Mykola Vashchuk <mmykolavashchuk@gmail.com>
2026-02-24 10:47:46 +0200

    feat: add exceptions and logging demo

--- src/main/java/PW_7/Task_4/Logger.java
+++
@@ -1,7 +0,0 @@
-package PW_7.Task_4;
-
-public interface Logger {
-    void info(String message, Object... args);
-    void warn(String message, Object... args);
-    void error(String message, Object... args);
-}
--- src/main/java/PW_7/Task_4/LoggerFactory.java
+++
@@ -1,7 +0,0 @@
-package PW_7.Task_4;
-
-public class LoggerFactory {
-    public static Logger getLogger() {
-        return new SimpleLogger();
-    }
-}
